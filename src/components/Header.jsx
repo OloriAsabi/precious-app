fc00c28b2ac56b105474846ddd1e51f4d9726021
OloriAsabi <developerqueenlatifa@gmail.com>
2022-09-09 17:52:02 +0100

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,47 @@
 import React from 'react'
-import { GoPrimitiveDot } from "react-icons/go";
 import { RiCopperCoinLine } from 'react-icons/ri';
 import { BsShieldFillCheck } from "react-icons/bs";
-import { useNavigate, useParams } from 'react-router-dom';
-import { Chart } from "react-google-charts";
+// import { useNavigate, useParams } from 'react-router-dom';
+import DonutChart from 'react-donut-chart';
 
 const Header = ({ data }) => {
 
-    const datas = [
-        ["Task", "Percentage"],
-        ["Margin", 3],
-        ['Gain', `${data?.gain_percentage}`],
-        // CSS-style declaration
+    const dataItems = [
+        {
+            label: "Gain",
+            name: "Gain",
+            value:`${data?.gain_percentage}`,
+            borderWidth: '1',
+            backgroundColor: '#0606d9',
+        },
+        {
+          label: "Margin",
+          name: "Margin",
+          value: 3,
+          borderWidth: '1',
+          backgroundColor: '#0606d9',
+        },
       ];
 
-      const options = {
-        title: "",
-        pieHole: 0.5,
-        legend: "none", 
-        pieSliceText: "label",
-        is3D: false,
-        slices: [
-            {
-              color: "#b6b6b6"
-            },
-            {
-              color: "#007fad"
-            },
-          ],
+      const legend = {
+            display: true,
+            position: "bottom",
+            usePointStyle: "true",
+            labels: {
+              fontSize: 12,
+              padding: 25,
+              fontColor: "#6D7278",
+            }
       };
+
   return (
     <div className='container mx-auto pt-5'>
-         <div  className='flex justify-between'>
+         <div  className='flex loaded cursor-pointer mb-10'>
             <h1 className='items-start font-bold uppercase'>Loaded Gain</h1>
             <button className='border-blue-400 border px-4 py-1 text-blue-400 '>Distribute</button>
         </div>
     
-        <div className='grid lg:grid-cols-3 sm:grid-cols-1 space-x-10 justify-between items-center'>
+        <div className='grid lg:grid-cols-3 sm:grid-cols-2 space-x-10 justify-between items-center'>
             <div className='shadow-transpent rounded-xl border border-10 w-full h-full p-5'>
                 <h2 className='font-bold uppercase pt-3'>weekly Trades For Week {data?.week_start} - {data?.week_end}</h2>
                 <div className='flex justify-between pt-3 pb-3'>
@@ -46,39 +51,19 @@ const Header = ({ data }) => {
 
                 <hr className=''/>
 
-                {/* <div className='items-center justify-center flex p-5'>
-                    <div className='rounded-full border-8 border-gray-100 border-spacing-5 p-20'>
-                        <h3 className='font-bold text-green-700 text-xl'>{data?.gain_percentage}</h3>
-                        <p className='text-xl  text-gray-500 pl-3'>Gain</p>
-                    </div>
-                </div> */}
-                <Chart
-                chartType="PieChart"
-                width="100%"
-                height="400px"
-                data={datas}
-                options={options}
-                />
-                <div className='flex justify-between pb-3'>
-                    <div className='flex'>
-                    <GoPrimitiveDot fontSize={18} className='mt-1 text-gray-400'/>
-                    <div className='pl-2 text-gray-500'>
-                    <p>Margin</p>
-                    <p>{data?.margin}</p>
-                    </div>
-                    </div>
-                    <div  className='flex'>
-                    <GoPrimitiveDot fontSize={18} className='mt-1 text-blue-500'/>
-                    <div className='pl-2 text-gray-500'>
-                    <p>Gain</p>
-                    <p>{data?.gain}</p>
-                    </div>
-                    </div>
-                </div>
-                <hr/>
+                <DonutChart
+                data={dataItems}
+                height={250}
+                width={380} 
+               colors={['#0606d9', '#b7b7b7']}
+               className='ml-auto mr-auto mt-12 mb-12 text-xl '
+               legend={legend}
+               />   
+              <hr/>
                 <p className='p-5 text-blue-400 text-xl'>View Full Report</p>
             </div>
             <div className='shadow-transpent rounded-xl border border-10 h-full p-5'>
+                
             <p className='font-bold text-gray-600 pt-3 text-xl'>Top traded position for the week</p>
 
 
